acfb0b1870f4318663eef89d19bc3a8214eec7ab
Eddie Chu <contact@eddiechu.me>
2019-07-05 18:21:31 -0700

    add register actions

--- src/actions/index.js
+++ src/actions/index.js
@@ -21,3 +21,44 @@ export const fetchIntros = () => dispatch => {
         })
         .catch(err => console.log(err));
 }
+
+export const REGISTER_USER_SUCCESS = "REGISTER_USER_SUCCESS";
+export const registerUserSuccess = success => ({
+    type: REGISTER_USER_SUCCESS,
+    success
+});
+
+export const registerUser = credentials => dispatch => {
+    const url = API + `/users/register`;
+
+    return (
+        fetch(url, {
+            method: "POST",
+            body: JSON.stringify(credentials),
+            headers: {
+                "Content-Type": "application/json"
+            }
+        })
+            //if succussful User persist to DB, automatically login the new User
+            .then(response => {
+                if (response.status === 201) {
+                    const loginCreds = {
+                        username: credentials.username,
+                        password: credentials.password
+                    };
+                    dispatch(loginUser(loginCreds));
+                }
+                // if no successful response return server response
+                else {
+                    return response
+                        .json()
+                        .then(response => {
+                            alert(`${response.location.toUpperCase()} ${response.message.toUpperCase()}`);
+                        })
+                        .catch(err => console.log(err));
+                }
+                return response.json();
+            })
+            .catch(err => console.log(err))
+    );
+};
--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,7 +1,53 @@
 import React from "react";
+import connect from "react-redux";
+import { registerUser } from "../actions/index";
 
-export default class Signup extends React.Component() {
+export class Signup extends React.Component() {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            email: "",
+            password: "",
+            firstname: "",
+            lastname: ""
+        }
+    }
+
+    handleRegister = e => {
+        e.preventDefault();
+        this.props.registerUser(this.state);
+    }
+
+
     render() {
-        return()
+        return(
+           <form onSubmit={this.handleRegister}>
+               <fieldset>
+                   <legend>Sign Up</legend>
+                   <label for="email">Email</label>
+                   <input id="email" type="email" onChange={e => this.setState({ email: e.target.value })} required></input>
+                   <label for="password" type="text">Password</label>
+                   <input id="password" type="text" onChange={e => this.setState({ password: e.target.value })} required></input>
+                   <label id="firstname">Firstname</label>
+                   <input id="firstname" type="text" onChange={e => this.setState({ firstname: e.target.value })}></input>
+                   <label id="lastname">Lastname</label>
+                   <input id="lastname" type="text" onChange={e => this.setState({ lastname: e.target.value })}></input>
+               </fieldset>
+           </form>
+        )
     }
 }
+
+const mapDispatchToProps = dispatch => {
+    return {
+        registerUser: userInfo => {
+            dispatch(registerUser(userInfo));
+        }
+    };
+};
+
+export default connect(
+    null,
+    mapDispatchToProps
+)(Signup);
